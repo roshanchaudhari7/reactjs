5977ae93b22fa58cba7a2f725b8629f5ff42647e
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2024-11-05 19:19:10 +0530

    components and props

---
+++ components1/src/Components/Buttons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Buttons = () => {
+  return (
+    <button>Click Me</button>
+  )
+}
+
+export default Buttons
---
+++ components1/src/Components/Example.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Example = () => {
+  return (
+    <div>
+      <h1>Hello from example components</h1>
+    </div>
+  )
+}
+
+export default Example
