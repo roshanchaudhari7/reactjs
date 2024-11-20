73100f94fa09aad4a8bb5efadcfb77ff7fe92d27
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2024-11-20 10:46:17 +0530

    Normalize line endings

--- useEffect-01/src/Components/ImagesCards.jsx
+++ useEffect-01/src/Components/ImagesCards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ImagesCards = ({name, image}) => {
   return (
     <div className='border border-black rounded-md w-[300px] flex flex-col items-center p-2 m-6'>
-      <img className=' w-[200px] h-[150px] rounded-md' src={image} alt="" />
+      <img className=' w-[200px] h-[150px] rounded-md' src={image} alt="images" />
       <p className='mt-2'>{name}</p>
     </div>
   )
