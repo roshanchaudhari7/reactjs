f6bda1982d2098e50218efd7a3baa04d3e27f087
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2025-04-05 08:44:44 +0530

    reactRouting

--- reactRouting-03/src/App.jsx
+++ reactRouting-03/src/App.jsx
@@ -8,36 +8,37 @@ import Home from './Components/Home'
 import Example from './Components/Example'
 const App = () => {
   return (
-    <div>
-      <Example />
-    </div>
-    // <>
-    //   <BrowserRouter>
-    //     <div style={{ backgroundColor: '#94fcd9', overflow: 'hidden', padding: '10px 20px' }}>
-    //       <Link to="/compa" style={{ textDecoration: 'none', padding: '8px 16px', fontSize: '18px', float: 'left' }}>
-    //         CompA
-    //       </Link>
-    //       <Link to="/compb" style={{ textDecoration: 'none', padding: '8px 16px', fontSize: '18px', float: 'left' }}>
-    //         CompB
-    //       </Link>
-    //       <Link to="/compc" style={{ textDecoration: 'none', padding: '8px 16px', fontSize: '18px', float: 'left' }}>
-    //         CompC
-    //       </Link>
-    //       <Link to="/compd" style={{ textDecoration: 'none', padding: '8px 16px', fontSize: '18px', float: 'left' }}>
-    //         CompD
-    //       </Link>
-    //     </div>
-    //     <div>
-    //       <Routes>
-    //         <Route path='/' element={<Home />} />
-    //         <Route path='/compa' element={<CompA />} />
-    //         <Route path='/compb' element={<CompB />} />
-    //         <Route path='/compc' element={<CompC />} />
-    //         <Route path='/compd' element={<CompD />} />
-    //       </Routes>
-    //     </div>
-    //   </BrowserRouter>
-    // </>
+    <>
+      <BrowserRouter>
+        <div style={{ backgroundColor: '#94fcd9', overflow: 'hidden', padding: '10px 20px' }}>
+          <Link to="/compa" style={{ textDecoration: 'none', padding: '8px 16px', fontSize: '18px', float: 'left' }}>
+            CompA
+          </Link>
+          <Link to="/compb" style={{ textDecoration: 'none', padding: '8px 16px', fontSize: '18px', float: 'left' }}>
+            CompB
+          </Link>
+          <Link to="/compc" style={{ textDecoration: 'none', padding: '8px 16px', fontSize: '18px', float: 'left' }}>
+            CompC
+          </Link>
+          <Link to="/compd" style={{ textDecoration: 'none', padding: '8px 16px', fontSize: '18px', float: 'left' }}>
+            CompD
+          </Link>
+          <Link to="/example" style={{ textDecoration: 'none', padding: '8px 16px', fontSize: '18px', float: 'left' }}>
+            Example
+          </Link>
+        </div>
+        <div>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/compa' element={<CompA />} />
+            <Route path='/compb' element={<CompB />} />
+            <Route path='/compc' element={<CompC />} />
+            <Route path='/compd' element={<CompD />} />
+            <Route path='/example' element={<Example/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   )
 }
 
